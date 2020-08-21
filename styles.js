ecdfe4567feaf80de19df188b04eb63b9770fb98
LekamCharity <lekamcharity@gmail.com>
2020-08-21 13:22:49 +0300

    added a function on my js

--- styles.js
+++ styles.js
@@ -7,7 +7,42 @@ function getInput() {
 
   if (century == "") {
     alert("Input correct gender");
-    return false;
+    return error;
+  } else if (year == "") {
+    alert("input correct year");
+    return error;
+  } else if (month == "") {
+    alert("input correct month");
+    return error;
+  } else if (dayOfMonth == "") {
+    alert("input correct day");
+    return error;
   }
+}
+function calculateDay(){
+  getInput();
+  dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
+  console.log(dayOfWeek);
+  return (Math.floor(dayOfWeek));
+}
 
+function checkDayOfWeek(){
+   day = calculateDay();
+    checkGender();
+    console.log("The function runs");
+}
+
+let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+
+function checkGender() {
+  var gen = document.getElementsByName("rads");
+  if (gen[0].checked == true) {
+    var gender = "male";
+  } else if (gen[1].checked == true) {
+    var gender = "female";
+  } else {
+    console.log("pass");
+  }
 }
